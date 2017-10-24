1384269c25f45cd328acc59c1786256ea911473b
Ask Solem <ask@celeryproject.org>
2017-10-24 12:33:53 -0700

    Forgot to add module mode.loop.eventlet

---
+++ mode/loop/eventlet.py
@@ -0,0 +1,25 @@
+import os
+os.environ['GEVENT_LOOP'] = 'mode.loop._gevent_loop.Loop'
+try:
+    import eventlet
+except ImportError:
+    raise ImportError(
+        'Eventlet loop requires the eventlet library: '
+        'pip install eventlet') from None
+eventlet.monkey_patch()
+
+try:
+    import aioeventlet
+except ImportError:
+    raise
+    raise ImportError(
+        'Eventlet loop requires the aioeventlet library: '
+        'pip install aioeventlet') from None
+
+import asyncio  # noqa: E402
+if asyncio._get_running_loop() is not None:
+    raise RuntimeError('Event loop created before importing eventlet loop!')
+
+Policy = aioeventlet.EventLoopPolicy
+policy = Policy()
+asyncio.set_event_loop_policy(policy)
