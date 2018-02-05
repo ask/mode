b93c8e7bcf88bc8dd28d6dfeb58abb543fd8dba8
Ask Solem <ask@celeryproject.org>
2018-02-05 12:09:27 -0800

    Moves utils.compat to mode

--- mode/utils/compat.py
+++ mode/utils/compat.py
@@ -4,7 +4,7 @@ import collections
 import typing
 from types import TracebackType
 from typing import (
-    Any, ContextManager, Dict, Generic, KT,
+    Any, AnyStr, ContextManager, Dict, Generic, IO, KT,
     MutableMapping, MutableSequence, Optional, T, T_co, Type, VT,
 )
 import _collections_abc
@@ -15,7 +15,42 @@ __all__ = [
     'Counter',
     'Deque',
     'DummyContext',
+    'OrderedDict',
+    'want_bytes',
+    'want_str',
+    'isatty',
 ]
+
+
+#: Dictionaries are ordered by default in Python 3.6
+OrderedDict = dict
+
+
+def want_bytes(s: AnyStr) -> bytes:
+    """Convert string to bytes."""
+    if isinstance(s, str):
+        return s.encode()
+    return s
+
+
+def want_str(s: AnyStr) -> str:
+    """Convert bytes to string."""
+    if isinstance(s, bytes):
+        return s.decode()
+    return s
+
+
+def isatty(fh: IO) -> bool:
+    """Return True if fh has a controlling terminal.
+
+    Notes:
+        Use with e.g. :data:`sys.stdin`.
+    """
+    try:
+        return fh.isatty()
+    except AttributeError:
+        return False
+
 
 if typing.TYPE_CHECKING:
     from typing import AsyncContextManager
