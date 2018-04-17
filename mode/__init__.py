aa4a13899e9a89d874c241c41976ee02c66d180f
Ask Solem <ask@celeryproject.org>
2018-04-17 13:25:38 -0700

    Bump version: 1.10.4 → 1.11.0

--- mode/__init__.py
+++ mode/__init__.py
@@ -8,7 +8,7 @@ import sys
 import typing
 from typing import Any, Mapping, NamedTuple, Sequence
 
-__version__ = '1.10.4'
+__version__ = '1.11.0'
 __author__ = 'Robinhood Markets'
 __contact__ = 'opensource@robinhood.com'
 __homepage__ = 'https://github.com/fauststream/mode'
