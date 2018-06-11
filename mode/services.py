43223b78616cfc2565589102b15f001478cfefdb
Ask Solem <ask@celeryproject.org>
2018-06-11 14:01:06 -0700

    Service.start: bail early when start/stop executed by different coroutines

--- mode/services.py
+++ mode/services.py
@@ -663,16 +663,26 @@ class Service(ServiceBase, ServiceCallbacks):
         if not self.restart_count:
             self._children.extend(self.on_init_dependencies())
             await self.on_first_start()
+        if self.should_stop:
+            return
         self.exit_stack.__enter__()
         await self.async_exit_stack.__aenter__()
         try:
+            if self.should_stop:
+                return
             self.log.info('Starting...')
             await self.on_start()
+            if self.should_stop:
+                return
             for task in self._get_tasks():
                 self.add_future(task.fun(self))
             for child in self._children:
                 if child is not None:
                     await child.maybe_start()
+                if self.should_stop:
+                    return
+            if self.should_stop:
+                return
             self.log.debug('Started.')
             await self.on_started()
         except BaseException as exc:
@@ -796,18 +806,18 @@ class Service(ServiceBase, ServiceCallbacks):
 
     async def restart(self) -> None:
         """Restart this service."""
-        self.restart_count += 1
-        await self.stop()
         self.service_reset()
         await self.on_restart()
         await self.start()
 
     def service_reset(self) -> None:
+        self.restart_count += 1
         for ev in (self._started,
                    self._stopped,
                    self._shutdown,
                    self._crashed):
             ev.clear()
+        await self.stop()
         self._crash_reason = None
         for child in self._children:
             if child is not None:
