18967b1834a7bc56348d9bf62ad4c480e08b14f3
Ask Solem <ask@celeryproject.org>
2018-09-07 16:12:57 -0700

    utils.mocks: Adds AsyncContextManagerMock

--- mode/utils/mocks.py
+++ mode/utils/mocks.py
@@ -6,6 +6,7 @@ from typing import Any
 __all__ = [
     'ANY',
     'AsyncMock',
+    'AsyncContextManagerMock',
     'FutureMock',
     'MagicMock',
     'Mock',
@@ -14,12 +15,35 @@ __all__ = [
 ]
 
 
-def AsyncMock(name: str = None, **kwargs: Any) -> unittest.mock.Mock:
-    coro = Mock(**kwargs)
-    corofun = Mock(name=name)
-    corofun.attach_mock(coro, 'coro')
-    corofun.side_effect = coroutine(coro)
-    return corofun
+class AsyncMock(unittest.mock.Mock):
+
+    def __init__(self, *args: Any,
+                 name: str = None,
+                 **kwargs: Any) -> None:
+        super().__init__(name=name)
+        coro = Mock(*args, **kwargs)
+        self.attach_mock(coro, 'coro')
+        self.side_effect = coroutine(coro)
+
+
+class AsyncContextManagerMock(unittest.mock.Mock):
+
+    def __init__(self, *args: Any,
+                 aenter_return: Any = None,
+                 aexit_return: Any = None,
+                 **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.aenter_return = aenter_return
+        self.aexit_return = aexit_return
+
+    async def __aenter__(self) -> Any:
+        mgr = self.aenter_return or self.return_value
+        if isinstance(mgr, AsyncMock):
+            return mgr.coro
+        return mgr
+
+    async def __aexit__(self, *args: Any) -> Any:
+        return self.aexit_return
 
 
 class FutureMock(unittest.mock.Mock):
