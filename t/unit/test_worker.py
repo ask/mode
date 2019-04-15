3422975f880c773be610f41752e44f3f3fd590be
Ask Solem <asksol@robinhood.com>
2019-04-14 20:21:19 -0700

    Attempt to fix test

--- t/unit/test_worker.py
+++ t/unit/test_worker.py
@@ -45,8 +45,8 @@ class test_Worker:
         stderr = Mock()
         assert Worker(stdout=stdout).stdout is stdout
         assert Worker(stderr=stderr).stderr is stderr
-        assert Worker(stdout=None).stdout is sys.stdout
-        assert Worker(stderr=None).stderr is sys.stderr
+        assert Worker(stdout=None).stdout is not sys.stdout
+        assert Worker(stderr=None).stderr is not sys.stderr
 
     def test_constructor_aligns_beacons(self):
         x = Service()
