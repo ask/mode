6807e048e86cb5f43c0cf2cef0976747bf4f8a5e
Ask Solem <asksol@robinhood.com>
2019-12-11 14:03:46 -0800

    CI: Adds Python 3.8.0 and 3.7.5

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ classes = """
     Programming Language :: Python :: 3 :: Only
     Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
     Programming Language :: Python :: Implementation :: CPython
     Operating System :: POSIX
     Operating System :: Microsoft :: Windows
